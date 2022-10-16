7f58b501cd1355cbc9537495a4b8d8b326eb9864
chrlr <yosoypro_117@hotmail.com>
2022-10-16 12:28:00 -0500

    progress bar

---
+++ python/progressBar.py
@@ -0,0 +1,15 @@
+import time
+
+def progressBar(part, total, length = 30):
+    frac = part / total
+    completed = int(frac * length)
+    missing = length - completed
+    bar = f"[{'#' * completed}{'-' * missing}]{frac:.2%}"
+
+    return bar
+
+n = 30
+
+for i in range(n + 1):
+    time.sleep(0.1)
+    print(progressBar(i, n), end='\r')
